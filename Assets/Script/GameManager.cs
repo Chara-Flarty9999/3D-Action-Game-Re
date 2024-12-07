3a72780fd2c30e54215c84d441a106701eb9e511
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2024-12-07 19:35:50 +0900

    [add]ゲームマネージャー作成開始

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    private float HP {get; set;}
+
+    private float MaxHP = 10;
+
+    UnityEvent _getDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
