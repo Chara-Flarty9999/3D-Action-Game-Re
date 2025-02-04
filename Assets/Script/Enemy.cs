45648c80d33196460d9682552b3c1b62d7f10722
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-02-04 15:26:12 +0900

    [add]敵の簡素システムの作成

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float _enemyLife = 20;
+    Rigidbody rb;
+    MeshRenderer mesh;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage_Heal(int change_HP)
+    {
+        _enemyLife += change_HP;
+        mesh.material.color = change_HP >= 0 ? new Color(0, 1, 0, 1) : new Color(1, 0, 0, 1);
+        if (_enemyLife <= 0)
+        {
+            Destroy(gameObject, 1f);
+        }
+        else
+        {
+            mesh.material.DOColor(new Color(1, 1, 1), 0.8f);
+        }
+    }
+}
