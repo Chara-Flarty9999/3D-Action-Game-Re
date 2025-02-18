f736e816797181a1f36d227b2f0bb69a829ebae8
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2025-02-18 19:54:02 +0900

    [add] 敵のタイプとして障害物型、追尾型、タレット型、一定移動型の四種の追加、タレットの弾丸発射のプログラムの追加

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -8,8 +8,30 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("タレット専用の設定")]
+    /// <summary>
+    /// 射出するオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("射出するオブジェクトをここに割り当てる(Normal以外)")]
+    private GameObject throwingObject;
 
+    /// <summary>
+    /// 標的のオブジェクト
+    /// </summary>
+    [SerializeField, Tooltip("標的のオブジェクトをここに割り当てる(Normal以外)")]
+    private GameObject targetObject;
+
+    /// <summary>
+    /// 射出角度
+    /// </summary>
+    [SerializeField, Range(0F, 90F), Tooltip("射出する角度(Turretのみ)")]
+    private float ThrowingAngle;
+
+    GameObject _turretBase;
+
+    [Header("共通設定")]
     [SerializeField] float _maxEenemyLife = 20;
+    [SerializeField] EnemyType _enemyType;
     float _enemyLife;
     Rigidbody rb;
     MeshRenderer mesh;
@@ -24,6 +46,7 @@ public class Enemy : MonoBehaviour
     {
         _enemyLife = _maxEenemyLife;
         enemyLifeGage = transform.Find("Canvas/EnemyLifeGageRoot/EnemyLifeGage").gameObject;
+        _turretBase = transform.Find("TurretBase").gameObject;
         enemyCanvas = transform.GetChild(0).gameObject;
         gage_image = enemyLifeGage.GetComponent<Image>();
         rb = GetComponent<Rigidbody>();
@@ -33,7 +56,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        switch (_enemyType)
+        {
+            case EnemyType.Turret:
+                Vector3 playerPos = targetObject.transform.position;
+                playerPos.y = transform.position.y;
+                _turretBase.transform.LookAt(playerPos);
+                break;
+        }
     }
 
     private void FixedUpdate()
@@ -57,8 +87,83 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// ボールを射出する
+    /// </summary>
+    private void ThrowingBall()
+    {
+        if (throwingObject != null && targetObject != null && _enemyType == EnemyType.Turret)
+        {
+            // Ballオブジェクトの生成
+            GameObject ball = Instantiate(throwingObject, this.transform.position, Quaternion.identity);
+
+            // 標的の座標
+            Vector3 targetPosition = targetObject.transform.position;
+
+            // 射出角度
+            float angle = ThrowingAngle;
+
+            // 射出速度を算出
+            Vector3 velocity = CalculateVelocity(this.transform.position, targetPosition, angle);
+
+            // 射出
+            Rigidbody rid = ball.GetComponent<Rigidbody>();
+            rid.AddForce(velocity * rid.mass, ForceMode.Impulse);
+        }
+        else
+        {
+            if (_enemyType == EnemyType.Turret)
+            {
+                throw new System.Exception("タレットではないため発射できません。");
+            }
+            else
+            {
+                throw new System.Exception("射出するオブジェクトまたは標的のオブジェクトが未設定です。");
+            }
+        }
+    }
+
+    /// <summary>
+    /// 標的に命中する射出速度の計算
+    /// </summary>
+    /// <param name="pointA">射出開始座標</param>
+    /// <param name="pointB">標的の座標</param>
+    /// <returns>射出速度</returns>
+    private Vector3 CalculateVelocity(Vector3 pointA, Vector3 pointB, float angle)
+    {
+        // 射出角をラジアンに変換
+        float rad = angle * Mathf.PI / 180;
+
+        // 水平方向の距離x
+        float x = Vector2.Distance(new Vector2(pointA.x, pointA.z), new Vector2(pointB.x, pointB.z));
+
+        // 垂直方向の距離y
+        float y = pointA.y - pointB.y;
+
+        // 斜方投射の公式を初速度について解く
+        float speed = Mathf.Sqrt(-Physics.gravity.y * Mathf.Pow(x, 2) / (2 * Mathf.Pow(Mathf.Cos(rad), 2) * (x * Mathf.Tan(rad) + y)));
+
+        if (float.IsNaN(speed))
+        {
+            // 条件を満たす初速を算出できなければVector3.zeroを返す
+            return Vector3.zero;
+        }
+        else
+        {
+            return (new Vector3(pointB.x - pointA.x, x * Mathf.Tan(rad), pointB.z - pointA.z).normalized * speed);
+        }
+    }
+
     private void OnDestroy()
     {
         GameManager.leftEnemyBox--;
+    }
+
+    enum EnemyType
+    {
+        Object,
+        Turret,
+        Wander,
+        chase
     }
 }
